2c75df83cbe3108a0dd99ea509399d01c267f003
Nicholas Pufal <npufal@doximity.com>
2022-04-01 10:48:52 -0700

    Add support to PR numbers

--- src/list_ref_files.js
+++ src/list_ref_files.js
@@ -1,28 +1,81 @@
 const axios = require('axios')
 
-async function fetchData({ repository, ref, token }) {
-  const httpClient = axios.create({
-    baseURL: 'https://api.github.com',
-    headers: {'Authorization': `Bearer ${token}`}
-  })
-  const response = await httpClient.get(`/repos/${repository}/commits/${ref}`)
+class baseStrategy {
+  constructor({ repository, ref, pr_number, token }) {
+    this.repository = repository
+    this.ref = ref
+    this.pr_number = pr_number
+    this.token = token
+  }
 
-  return response.data
+  async filenames() {
+    const payload = await this.fetchPayload()
+    return this.filterPayload(payload)
+  }
+
+  filterPayload() {
+    throw new Error("Implement me!")
+  }
+
+  async fetchPayload() {
+    throw new Error("Implement me!")
+  }
 }
 
-async function filterPayload(payload) {
-  return payload["files"].map((fileMetadata) => {
-    return fileMetadata["filename"]
-  })
+class pullRequestStrategy extends baseStrategy {
+  filterPayload(payload) {
+    return payload.map((item) => {
+      return item["filename"]
+    })
+  }
+
+  async fetchPayload() {
+    const httpClient = axios.create({
+      baseURL: 'https://api.github.com',
+      headers: {'Authorization': `Bearer ${this.token}`}
+    })
+    const response = await httpClient.get(`/repos/${this.repository}/pulls/${this.pr_number}/files?per_page=100`)
+    return response.data
+  }
 }
 
-async function listRefFiles(inputs = {}) {
-  const payload = await fetchData(inputs)
-  const filesList = filterPayload(payload)
+class refStrategy {
+  filterPayload(payload) {
+    return payload["files"].map((fileMetadata) => {
+      return fileMetadata["filename"]
+    })
+  }
 
-  return filesList
+  async fetchPayload() {
+    const httpClient = axios.create({
+      baseURL: 'https://api.github.com',
+      headers: {'Authorization': `Bearer ${this.token}`}
+    })
+    const response = await httpClient.get(`/repos/${this.repository}/commits/${this.ref}?per_page=100`)
+    return response.data
+  }
+}
+
+function validateInputs(inputs) {
+  if (inputs.ref && inputs.pr_number) {
+    throw new Error("The options ref and pr_number are mutually exclusive — pick one!")
+  }
+
+  if ((inputs.ref === undefined) && (inputs.pr_number === undefined)) {
+    throw new Error("You have to specify either ref or pr_number for this action to work")
+  }
+}
+
+async function listFilenames(inputs) {
+  validateInputs(inputs)
+
+  if (inputs.pr_number) {
+    return await new pullRequestStrategy(inputs).filenames()
+  } else {
+    return await new refStrategy(inputs).filenames()
+  }
 }
 
 module.exports = {
-  listRefFiles
+  listFilenames
 }
